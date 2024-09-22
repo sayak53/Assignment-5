243c7d7a37d13d5b587304d151606b6cd4f85967
sayak53 <sayakchakraborty928@gmail.com>
2024-09-22 20:54:12 +0530

    logic created succesfully

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.getElementById('history-button').addEventListener('click',function(){
+    const donation = document.getElementById('donation')
+    donation.setAttribute('class','hidden')
+
+    const history = document.getElementById('history')
+    history.setAttribute('class','block')
+
+    document.getElementById('history-button').classList.add('green-background')
+    document.getElementById('donation-button').classList.remove('green-background')
+})
+
+document.getElementById('donation-button').addEventListener('click',function(){
+    const donation = document.getElementById('donation')
+    donation.setAttribute('class','block')
+
+    const history = document.getElementById('history')
+    history.setAttribute('class','hidden')
+
+    document.getElementById('history-button').classList.remove('green-background')
+    document.getElementById('donation-button').classList.add('green-background')
+})
